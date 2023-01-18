528f529fbe369f3fc5f6f90da88315d3ce395536
Cassandra Keddis <cassandra@thinkific.com>
2023-01-17 21:04:31 -0800

    refactor

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,16 +1,15 @@
-import matchData from '../data/matchData';
-import Match from "./Match";
+import Match from './Match';
 
 function MatchList(props) {
 
-  const oneMatch = matchData[0];
+    const oneMatch = props.matchData[0];
 
-  return (
-    <section className="PlayerList MatchList">
-      <h1>Match list</h1>
-      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
-    </section>
-  );
-}
+    return (
+      <section className="PlayerList MatchList">
+        <h1>Match list</h1>
+        <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
+      </section>
+    );
+  }
 
 export default MatchList;
