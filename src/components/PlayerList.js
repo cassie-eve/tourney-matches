12e4561c873facefd4a74e88d32089175fbfb2b5
Cassandra Keddis <cassandra@thinkific.com>
2023-01-17 19:33:15 -0800

    create PlayerList component

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,4 +1,12 @@
-<section className="PlayerList">
-  <h1>Current participating players</h1>
-  {/* Players will be shown here */}
-</section>
+import Player from "./Player";
+
+function PlayerList() {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      <Player />
+    </section>
+  );
+}
+
+export default PlayerList;
